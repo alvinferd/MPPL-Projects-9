2245a2d8a5689131f56eccfc19c2b6760e393da3
Berly Setiawan <berly_set22@apps.ipb.ac.id>
2021-12-02 14:09:06 +0700

    tambah produk work but dont have feedback

--- frontend/utils/redux/slice/product.js
+++ frontend/utils/redux/slice/product.js
@@ -2,6 +2,7 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import baseApi from '../../api';
 import { loadingSet } from './loading';
 import { alertSetError, alertSetMessage, alertSetSuccess } from './alert';
+import router from 'next/router';
 
 const productSlice = createSlice({
     name: 'product',
@@ -38,3 +39,61 @@ export const readMyProduct = createAsyncThunk(
             })
     }
 )
+
+export const addNewProduct = createAsyncThunk(
+    'product/addNewProduct',
+    async (data, { dispatch }) => {
+        dispatch(loadingSet(true));
+        return baseApi
+            .post("/api/v1/product/addProduct", data)
+            .then((res) => {
+                // console.log(res);
+                dispatch(alertSetSuccess(true));
+                dispatch(alertSetMessage("Produk berhasil ditambahkan!"));
+                // dispatch(readMyProduct());
+            })
+            .catch((err) => {
+                // console.log(err);
+                dispatch(alertSetError(true));
+                dispatch(alertSetMessage(err.message));
+            })
+            .finally(() => {
+                dispatch(loadingSet(false));
+            })
+    }
+)
+
+// export const successAddProduct = createAsyncThunk(
+//     'product/successAddProduct',
+//     async (data, { dispatch }) => {
+//         // dispatch(loadingSet(true));
+//         dispatch(alertSetSuccess(true));
+//         dispatch(alertSetMessage("Produk berhasil ditambahkan!"));
+//     }
+// )
+
+
+// export const addNewProduct = createAsyncThunk(
+//     'product/addNewProduct',
+//     async (data, { dispatch }) => {
+//         const token = Cookies.get(TOKEN_KEY);
+//         // dispatch(loadingSet(true));
+//         return axios({
+//             method: "post",
+//             url: "http://103.41.205.191:10001/api/v1/product/addProduct",
+//             data: data,
+//             headers: {
+//                 "Content-Type": "multipart/form-data",
+//                 "Authorization": token,
+//             },
+//         })
+//             .then(function (response) {
+//                 //handle success
+//                 console.log(response);
+//             })
+//             .catch(function (err) {
+//                 //handle error
+//                 console.log("error");
+//             });
+//     }
+// )
